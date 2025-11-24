60733c38e34ccb78e444058d3f5c2e4a66a6b1b3
Roshnab Afraz <roshnabafrazz@gmail.com>
2025-11-24 19:36:01 +0500

    added Firebase SDK

--- app/src/main/java/com/corvit/corvit_lms/screens/splash_screen.kt
+++ app/src/main/java/com/corvit/corvit_lms/screens/splash_screen.kt
@@ -1,11 +1,9 @@
 package com.corvit.corvit_lms.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview
